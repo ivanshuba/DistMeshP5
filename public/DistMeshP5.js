50ea164cbffb921b3a55ffc9410c9d025e67cabd
ivanshuba <shubaivan@gmail.com>
2019-09-27 02:00:10 +0300

    Move *.js files into 'public' folder

--- public/DistMeshP5.js
+++ public/DistMeshP5.js
@@ -1,17 +1,76 @@
-var p1, p2, p3;
+var pts = [];
+var delaunay;
+var prevTime;
+let mousePos = [];
 
 function setup() {
-  createCanvas(500, 500);
-  var x0 = width * 0.5;
-  var y0 = height * 0.5;
-  p1 = new TPoint(x0 - 40, y0 + 0);
-  p2 = new TPoint(x0 + 0, y0 + 40);
-  p3 = new TPoint(x0 + 40, y0 + 0);
-  t = new Triangle(p1, p2, p3);
+  createCanvas(600, 400);
+  delaunay = new Delaunay();
+  prevTime = millis();
+
+  prefill();
 }
 
 function draw() {
   background(230);
-  t.draw();
+  textSize(20);
+  textAlign(CENTER, CENTER);
+  text("Click left mouse button on canvas", width * 0.5, 20);
+  text("Pres 't' for label drawing", width * 0.5, 40);
+
+  delaunay.draw();
+
+  fill(0);
+  textSize(12);
+  textAlign(LEFT, CENTER);
+  text("points:" + delaunay.points.length, 10, 15);
+  text("edges:" + delaunay.edges.length, 10, 30);
+  text("triangles:" + delaunay.triangles.length, 10, 45);
+  text("mousePos:" + mousePos[0] + ":" + mousePos[1], 10, 60);
+
+  if (keyIsPressed) {
+    if (key == 't') {
+      if (millis() - prevTime > 200) {
+        print("Ok");
+        delaunay.drawText = !delaunay.drawText;
+        prevTime = millis();
+      }
+    }
+  }
+
 }
 
+function mousePressed() {
+  if (mouseButton == LEFT) {
+    if (insideScreen(mouseX, mouseY)) {
+      if (millis() - prevTime > 300) {
+        prevTime = millis();
+        pts.push(new TPoint(mouseX, mouseY));
+        delaunay.triangulate(pts);
+      }
+    }
+  }
+}
+
+function keyPressed() {
+  if (key == 't') {
+    print("Ok");
+    delaunay.drawText = !delaunay.drawText;
+  }
+}
+
+function mouseMoved() {
+  mousePos[0] = mouseX;
+  mousePos[1] = mouseY;
+}
+
+function insideScreen(x, y) {
+  return (x > 0 && x < width && y > 0 && y < height);
+}
+
+function prefill() {
+  pts.push(new TPoint(width * 0.5 - 150, height * 0.5));
+  pts.push(new TPoint(width * 0.5, height * 0.5 + 150));
+  pts.push(new TPoint(width * 0.5 + 150, height * 0.5));
+  delaunay.triangulate(pts);
+}
--- public/TEdge.js
+++ public/TEdge.js
@@ -3,4 +3,17 @@ class TEdge {
     this.p1 = p1;
     this.p2 = p2;
   }
+  
+  draw() {
+    if (delaunay.drawText) {
+      fill(0);
+      textSize(12);
+      var label = delaunay.edges.indexOf(this) + ', [' + 
+                  delaunay.points.indexOf(this.p1) + ',' +
+                  delaunay.points.indexOf(this.p2) + ']';
+      var x = (this.p1.position.x + this.p2.position.x ) * 0.5;
+      var y = (this.p1.position.y + this.p2.position.y ) * 0.5;
+      text(label, x, y - 10);
+    }
+  }  
 }
